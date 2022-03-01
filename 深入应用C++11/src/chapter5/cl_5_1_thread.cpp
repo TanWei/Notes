2cd5eeee769be3b280dd42e866eb69389c9badbb
lei89 <dcsvgwudi@gmail.com>
2022-03-02 02:00:26 +0800

    内存屏障

--- 深入应用C++11/src/chapter5/cl_5_1_thread.cpp
+++ 深入应用C++11/src/chapter5/cl_5_1_thread.cpp
@@ -34,6 +34,43 @@ private:
     singleton operator=(const singleton&) = delete;
     static singleton* instance;
 };
+
+std::atomic<Singleton*> Singleton::m_instance;
+std::mutex Singleton::m_mutex;
+ 
+Singleton* Singleton::getInstance() {
+    Singleton* tmp = m_instance.load(std::memory_order_relaxed);
+    // 防止fence后的操作，重排到fence前的任意load之前
+    std::atomic_thread_fence(std::memory_order_acquire); 
+    if (tmp == nullptr) { // 问题：内存重排new，导致tmp不等于nullptr，但可能没初始化完成
+        std::lock_guard<std::mutex> lock(m_mutex);
+        tmp = m_instance.load(std::memory_order_relaxed);
+        if (tmp == nullptr) {
+            tmp = new Singleton;
+            // 防止fence前的操作，重排到fence后的任意store之后
+            std::atomic_thread_fence(std::memory_order_release); 
+            m_instance.store(tmp, std::memory_order_relaxed);
+        }
+    }
+    return tmp;
+}
+
+// std::atomic<Singleton*> Singleton::m_instance;
+// std::mutex Singleton::m_mutex;
+ 
+// Singleton* Singleton::getInstance() {
+//     Singleton* tmp = m_instance.load();
+//     if (tmp == nullptr) {
+//         std::lock_guard<std::mutex> lock(m_mutex);
+//         tmp = m_instance.load();
+//         if (tmp == nullptr) {
+//             tmp = new Singleton;
+//             m_instance.store(tmp);
+//         }
+//     }
+//     return tmp;
+// }
+
 struct A
 {
     private:
@@ -49,6 +86,8 @@ private:
 
 int main()
 {
+    std::atomic<int> at;
+    at.store(0);
     using T = B*;
     bool val = std::is_trivially_copyable<T>::value;
     val = std::is_copy_constructible<T>::value;
